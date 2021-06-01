f315438dd0583592ceaa1d06f60fc73f49e096c5
chensj <1441805785@qq.com>
2021-06-01 16:01:46 +0800

    11

--- components/modal/modal.vue
+++ components/modal/modal.vue
@@ -4,16 +4,16 @@
 			<view style="height: 100%; overflow-y: scroll;">
 				<view class="title"><text>活动说明</text></view>
 				<view class="red-text"><text>请各位跑友熟悉完赛规则，以下完赛要求，缺一不可:</text></view>
-				<view><text>1.请在9月20日24:00前发送完赛认证，之后不接受登记，无完赛认证无法参与奖品抽奖；</text></view>
+				<view><text>1. 请在8月20日24:00前发送完赛认证，之后不接受登记，无完赛认证无法参与奖品抽奖；</text></view>
 				<view><text>2.完赛:</text></view>
 				<view><text>三等奖奖品</text></view>
-				<view><text>7月20日~9月20日活动期间内，任意时间任意地点、任意运动APP≤5次记录完成13公里跑步距离，即可参与三等奖奖品抽奖资格；</text></view>
+				<view><text>6月20日~8月20日活动期间内，任意时间任意地点、任意运动APP≤5次记录完成13公里跑步距离，即可参与三等奖奖品抽奖资格；</text></view>
 				<view><text>二等奖奖品</text></view>
-				<view><text>7月20日~9月20日活动期间内，任意时间任意地点、任意运动APP≤3次记录完成13公里跑步距离，即可参与二等奖奖品抽奖资格；</text></view>
+				<view><text>6月20日~8月20日活动期间内，任意时间任意地点、任意运动APP≤3次记录完成13公里跑步距离，即可参与二等奖奖品抽奖资格；</text></view>
 				<view><text>一等奖奖品</text></view>
-				<view><text>7月20日~9月20日活动期间内，任意时间任意地点、任意运动APP≤1次记录完成13公里跑步距离，即可参与一等奖奖品抽奖资格；</text></view>
+				<view><text>6月20日~8月20日活动期间内，任意时间任意地点、任意运动APP≤1次记录完成13公里跑步距离，即可参与一等奖奖品抽奖资格；</text></view>
 				<view><text>3.完赛认证: 完成后即时将运动轨迹(显示日期距离)或活动视频(航展祝福打Call视频)，上传至报名系统APP，文字内容自创（每个参赛者单独发, 不能一人代替多人发) ；</text></view>
-				<view><text>4.活动结束后3个工作日内(即9月23日前)公示中奖名单，活动方客服人员会通知中奖者领取奖品；</text></view>
+				<view><text>4.活动结束后5个工作日内(即8月25日前)公示中奖名单，活动方客服人员会通知中奖者领取奖品；</text></view>
 				<view><text>5.免责说明: 奖品图片仅为效果展示，最终以活动方提供的奖品实物为准。</text></view>
 			</view>
 			<view class="close" @click="closeModal"><text>X</text></view>
